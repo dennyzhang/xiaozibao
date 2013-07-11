b4d1df2f76f81916d997dd6a0f5548e2dfe14072
filebat <filebat.mark@gmail.com>
2013-07-11 18:19:31 +0800

    change webserver port

--- code/show_article/webserver/config.py
+++ code/show_article/webserver/config.py
@@ -7,7 +7,7 @@
 ## Description :
 ## --
 ## Created : <2013-01-25 00:00:00>
-## Updated: Time-stamp: <2013-07-11 18:03:19>
+## Updated: Time-stamp: <2013-07-11 18:18:42>
 ##-------------------------------------------------------------------
 ################## global variable   #####################
 import os
@@ -16,7 +16,7 @@ DB_HOST="127.0.0.1"
 DB_USERNAME="user_2013"
 DB_PWD="ilovechina"
 DB_NAME="xzb"
-FLASK_SERVER_PORT="8081"
+FLASK_SERVER_PORT="9080"
 
 DATA_BASEDIR = os.getcwd()+"/../../../webcrawler_data"
 
