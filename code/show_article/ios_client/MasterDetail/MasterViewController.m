5092439473acc33ab7393376cecbc0fd7ab98e81
filebat <filebat.mark@gmail.com>
2013-07-20 09:22:25 +0800

    update document

--- code/show_article/ios_client/MasterDetail/MasterViewController.m
+++ code/show_article/ios_client/MasterDetail/MasterViewController.m
@@ -25,8 +25,9 @@
 {
     //NSURL *url = [NSURL URLWithString:@"http://httpbin.org/ip"];
 
-    NSString *urlPrefix=@"http://173.255.227.47:9080/";
+    //NSString *urlPrefix=@"http://173.255.227.47:9080/";
     //NSString *urlPrefix=@"http://127.0.0.1:9080/";
+    NSString *urlPrefix=@"http://192.168.100.101:9080/";
     NSString *urlStr= [urlPrefix stringByAppendingString:@"api_list_user_topic?uid=denny&topic=idea"];
     NSURL *url = [NSURL URLWithString:urlStr];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
