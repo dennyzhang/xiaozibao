64987e1c14059b9d7ea53fede629895cfab5830c
filebat <filebat.mark@gmail.com>
2013-08-09 15:41:43 +0800

    mq_tool support fetch one message

--- code/tool/xzb_mq_tool.py
+++ code/tool/xzb_mq_tool.py
@@ -6,7 +6,7 @@
 ## Description :
 ## --
 ## Created : <2013-02-14>
-## Updated: Time-stamp: <2013-08-09 15:38:49>
+## Updated: Time-stamp: <2013-08-09 15:40:59>
 ##-------------------------------------------------------------------
 import pika
 import sys
@@ -46,16 +46,36 @@ def get_message(queue_name):
 
 	channel.start_consuming()
 	
+def get_one_message(queue_name):
+	connection = pika.BlockingConnection(pika.ConnectionParameters(host=MQ_SERVER))
+	channel = connection.channel()
+
+	channel.queue_declare(queue=queue_name, durable=True)
+	print ' [*] Waiting for messages. To exit press CTRL+C'
+
+	channel.basic_qos(prefetch_count=1)
+	channel.basic_consume(callback_one, queue=queue_name)
+
+	channel.start_consuming()
+
 def callback(ch, method, properties, body):
 	print " [x] Received %r" % (body,)
 	# time.sleep( body.count('.') )
 	print " [x] Done"
 	ch.basic_ack(delivery_tag = method.delivery_tag)
 
+def callback_one(ch, method, properties, body):
+	print " [x] Received %r" % (body,)
+	# time.sleep( body.count('.') )
+	print " [x] Done"
+	ch.basic_ack(delivery_tag = method.delivery_tag)
+	sys.exit(0)
+
 def usage():
 	_usage = """Usage: %s <ACTION> <DATA>
 # %s insert queue_name sudo xzb_fetch_url.sh -f http://haowenz.com/a/bl/2013/2608.html -d webcrawler_raw_haowenz
 # %s get snake_worker-shell#1#d1#haowenz.com
+# %s getone snake_worker-shell#1#d1#haowenz.com
 """
 	_name_filler = (os.path.basename(sys.argv[0]),) * 3
 
@@ -83,6 +103,10 @@ if __name__ == "__main__":
 			queue_name = sys.argv[2]
 			get_message(queue_name)
 		else:
-			print "Error unknown command:" + str(sys.argv)
+			if sys.argv[1] == "getone":
+				queue_name = sys.argv[2]
+				get_one_message(queue_name)
+			else:
+				print "Error unknown command:" + str(sys.argv)
 
 ## File : xzb_mq_tool ends
