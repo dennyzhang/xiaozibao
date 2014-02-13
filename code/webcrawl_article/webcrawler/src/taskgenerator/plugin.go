93ee45ab75aec9c53f7669d1e202e385f0afa98e
DennyZhang <filebat.mark@gmail.com>
2014-02-13 13:48:08 -0500

    reduce code duplication of plugin

--- code/webcrawl_article/webcrawler/src/taskgenerator/plugin.go
+++ code/webcrawl_article/webcrawler/src/taskgenerator/plugin.go
@@ -7,35 +7,47 @@ import (
 )
 
 var generator = map[string] Stringy {
-	"^http://www.zhihu.com/topic/[0-9]+/top-answers.*$": zhihucom_1,
-	"^http://www.douban.com/group/.*/discussion\\?.*$": douban_1,
 	"^http://haowenz.com/a/[a-z]+/.*": haowenzcom_1,
-	"^http://svbtle.com/$": svbtlecom_1,
 	"^http://techcrunch.com/$": techcrunch_1,
 	"^http://techcrunch.com/.*$": techcrunch_2,
-	"^http://news.ycombinator.com/$": newsycombinator_1,
-	"^http://www.careercup.com/page\\?pid=.*": careercup_1,
 	"^http://.*.stackexchange.com/[^\\/]*$": stackexchange_1,
 	"^http://stackoverflow.com/questions/tagged/.*$": stackoverflow_1,
 
+	"^http://svbtle.com/$": func(url string) []Task { return generator_simple(url,
+			"<a href=\"http://(.*)\" class=\"article_link\">", "http://")},
+
 	"^http://www.chineseinla.com/today_highlight.*$": func(url string) []Task { return generator_simple(url,
-			"<dt><a href=\"(http://www.chineseinla.com/f/page_viewtopic/t_[0-9]+.html)\"")},
+			"<dt><a href=\"(http://www.chineseinla.com/f/page_viewtopic/t_[0-9]+.html)\"", "")},
 
 	"^http://www.zreading.cn/.*$": func(url string) []Task { return generator_simple(url,
-			"<a href=\"(http://www.zreading.cn/archives/[0-9]+.html)\"")},
+			"<a href=\"(http://www.zreading.cn/archives/[0-9]+.html)\"", "")},
+
+	"^http://news.ycombinator.com/$": func(url string) []Task { return generator_simple(url,
+			"<td class=\"title\"><a href=\"([^\"]*)\"", "")},
+
+	"^http://www.zhihu.com/topic/[0-9]+/top-answers.*$": func(url string) []Task { return generator_simple(url,
+			"<a class=\"question_link\" .*href=\"/question/([^\"]*)\"", "http://www.zhihu.com/question/")},
+
+	"^http://www.douban.com/group/.*/discussion\\?.*$": func(url string) []Task { return generator_simple(url,
+			"<a href=\"http://www.douban.com/group/topic/([0-9]+)/\"", "http://www.douban.com/group/topic/")},
 
 	"^http://zenhabits.net/archives/$": func(url string) []Task { return generator_simple(url,
-			"<a href=\"(http://zenhabits.net/[^\"]+)\">") },
+			"<a href=\"(http://zenhabits.net/[^\"]+)\">", "")},
 
 	"^http://www.36kr.com/feed$": func(url string) []Task { return generator_simple(url,
-			"<link>(http://www.36kr.com/p/[0-9]*.html)</link>") },
+			"<link>(http://www.36kr.com/p/[0-9]*.html)</link>", "")},
 
 	"^http://www.geekpreneur.com/page/.*$": func(url string) []Task { return generator_simple(url,
-			"<span class=\"title\"><a href=\"(http://www.geekpreneur.com/[^\"]+)\" ")},
+			"<span class=\"title\"><a href=\"(http://www.geekpreneur.com/[^\"]+)\" ", "")},
 
+	"^http://www.v2ex.com/go/.*$": func(url string) []Task { return generator_simple(url,
+			"<div class=\"cell .* t_(.*)\">", "http://www.v2ex.com/t/")},
+
+	"^http://www.careercup.com/page\\?pid=.*$": func(url string) []Task { return generator_simple(url,
+			"<div class=\"votesWrapper votesWrapperQuestion\" id=\"votes([^\"]*)\"", "http://www.careercup.com/question?id=")},
 }
 
-func generator_simple(url string, link_pattern string) []Task {
+func generator_simple(url string, link_pattern string, result_prefix string) []Task {
 	tasks := make([]Task, 0)
         _, content := webcrawler.Webcrawler(url)
 	//fmt.Print(content)
@@ -44,7 +56,7 @@ func generator_simple(url string, link_pattern string) []Task {
 	//fmt.Print(match_strings)
         for i := range match_strings {
 		record_string := match_strings[i]
-		tasks = append(tasks, Task{record_string[1]})
+		tasks = append(tasks, Task{result_prefix+record_string[1]})
         }
 
 	//fmt.Print(tasks)
@@ -65,7 +77,7 @@ func haowenzcom_1(url string) []Task {
 		tasks = append(tasks, Task{url_match_record[0][1]})
         }
 
-	//fmt.Print(tasks)
+	fmt.Print(tasks)
 
         return tasks
 }
@@ -122,25 +134,6 @@ func stackoverflow_1(url string) []Task {
         return tasks
 }
 
-
-func svbtlecom_1(url string) []Task {
-	tasks := make([]Task, 0)
-        _, content := webcrawler.Webcrawler(url)
-
-        content = webcrawler.Filter(content,"<body", "</body>")
-
-	link_pattern :=  "<a href=\"http://(.*)\" class=\"article_link\">"
-        match_strings := regexp.MustCompile(link_pattern).FindAllStringSubmatch(content, -1)
-        for i := range match_strings {
-		record_string := match_strings[i]
-		tasks = append(tasks, Task{"http://" + record_string[1]})
-        }
-
-	//fmt.Print(tasks)
-
-        return tasks
-}
-
 func techcrunch_1(url string) []Task {
 	fmt.Print("here: " + url + "\n")
 	tasks := make([]Task, 0)
@@ -177,83 +170,6 @@ func techcrunch_2(url string) []Task {
         }
 
 	//fmt.Print(tasks)
-
-        return tasks
-}
-
-func newsycombinator_1(url string) []Task {
-	tasks := make([]Task, 0)
-        _, content := webcrawler.Webcrawler(url)
-
-        content = webcrawler.Filter(content,"<body", "</body>")
-
-	link_pattern :=  "<td class=\"title\"><a href=\"([^\"]*)\""
-        match_strings := regexp.MustCompile(link_pattern).FindAllStringSubmatch(content, -1)
-        for i := range match_strings {
-		record_string := match_strings[i]
-		tasks = append(tasks, Task{record_string[1]})
-        }
-
-	//fmt.Print(tasks)
-
-        return tasks
-}
-
-func careercup_1(url string) []Task {
-	tasks := make([]Task, 0)
-        _, content := webcrawler.Webcrawler(url)
-
-        content = webcrawler.Filter(content,"<body", "</body>")
-
-	link_pattern :=  "<div class=\"votesWrapper votesWrapperQuestion\" id=\"votes([^\"]*)\""
-        match_strings := regexp.MustCompile(link_pattern).FindAllStringSubmatch(content, -1)
-        for i := range match_strings {
-		record_string := match_strings[i]
-		tasks = append(tasks, Task{"http://www.careercup.com/question?id=" + record_string[1]})
-        }
-
-	//fmt.Print(tasks)
-
-        return tasks
-}
-
-func zhihucom_1(url string) []Task {
-	tasks := make([]Task, 0)
-        _, content := webcrawler.Webcrawler(url)
-
-        content = webcrawler.Filter(content,"<body", "</body>")
-
-	//fmt.Print(content)
-
-	link_pattern :=  "<a class=\"question_link\" .*href=\"/question/([^\"]*)\""
-	// TODO: assertion if pattern match fail
-        match_strings := regexp.MustCompile(link_pattern).FindAllStringSubmatch(content, -1)
-        for i := range match_strings {
-		record_string := match_strings[i]
-		tasks = append(tasks, Task{"http://www.zhihu.com/question/" + record_string[1]})
-        }
-
-	//fmt.Print(tasks)
-
-        return tasks
-}
-
-func douban_1(url string) []Task {
-	tasks := make([]Task, 0)
-        _, content := webcrawler.Webcrawler(url)
-
-        content = webcrawler.Filter(content,"<body", "</body>")
-
-	//fmt.Print(content)
-
-	link_pattern :=  "<a href=\"http://www.douban.com/group/topic/([0-9]+)/\""
-        match_strings := regexp.MustCompile(link_pattern).FindAllStringSubmatch(content, -1)
-        for i := range match_strings {
-		record_string := match_strings[i]
-		tasks = append(tasks, Task{"http://www.douban.com/group/topic/" + record_string[1]})
-        }
-
-	fmt.Print(tasks)
 
         return tasks
 }
