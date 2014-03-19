14dedc058a0096f7e6793c51ce8a670b6aafe8a3
DennyZhang <filebat.mark@gmail.com>
2014-03-19 11:33:41 -0400

    misc

--- code/tool/crontab_update.sh
+++ code/tool/crontab_update.sh
@@ -1,4 +1,4 @@
-# 10,40 * * * * source /etc/profile; ($XZB_HOME/code/tool/crontab_update.sh 2>&1) 1>> $XZB_HOME/cron.log
+# 10,40 * * * * source /etc/profile; (sh $XZB_HOME/code/tool/crontab_update.sh 2>&1) 1>> $XZB_HOME/cron.log
 
 git pull
 git commit -am "schedule commit" && git push || true
