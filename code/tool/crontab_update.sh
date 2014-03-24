1038bb08c6fec714ab9456b8bd9ed49f89424b5d
DennyZhang <filebat.mark@gmail.com>
2014-03-23 23:04:51 -0400

    update cron

--- code/tool/crontab_update.sh
+++ code/tool/crontab_update.sh
@@ -1,6 +1,12 @@
 # 10,40 * * * * source /etc/profile; (sh $XZB_HOME/code/tool/crontab_update.sh 2>&1) 1>> $XZB_HOME/cron.log
 
 cd $XZB_HOME
-git pull
+(git pull | grep feedback.log) && should_restart="yes"
 git commit -am "schedule commit" && git push || true
 make update_feedback
+
+if [[ "$should_restart" = "yes" ]]; then
+    pid=$(lsof -i tcp:9180 | grep LISTEN | awk -F' ' '{print $2}')
+    [ -n $pid ] && kill -9 $pid
+    make start
+fi;
