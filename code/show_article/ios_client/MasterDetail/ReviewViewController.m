b35426fca32e0bf0911f18e3ae7184abdbd9f7bd
DennyZhang <filebat.mark@gmail.com>
2014-03-28 23:09:42 -0400

    cosmetic change

--- code/show_article/ios_client/MasterDetail/ReviewViewController.m
+++ code/show_article/ios_client/MasterDetail/ReviewViewController.m
@@ -106,7 +106,7 @@
 -(IBAction) barButtonEvent:(id)sender
 {
   // TODO
-  NSString* msg = @"Coming Soon. \nShare to friends, Or\nto twitter, wechat, etc";
+  NSString* msg = @"Coming Soon.\n\nShare to friends, Or\n twitter, wechat, etc";
   [ComponentUtil infoMessage:nil msg:msg];
 }
 
