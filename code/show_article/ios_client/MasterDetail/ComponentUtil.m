af3d2c7ad90220499ab5c5ccee540f349b9edf4d
DennyZhang <filebat.mark@gmail.com>
2014-04-15 21:54:22 -0400

    add my id to prevent mixpanel

--- code/show_article/ios_client/MasterDetail/ComponentUtil.m
+++ code/show_article/ios_client/MasterDetail/ComponentUtil.m
@@ -244,10 +244,10 @@
 
 + (BOOL)shouldMixpanel
 {
-  if ([[NSUserDefaults standardUserDefaults] integerForKey:@"IsEditorMode"] == 1) {
+  if ([[ComponentUtil:getUserId] isEqualToString:@"AA6B312A-E380-477A-A593-3F9484C39E59"])
     return NO;
-  }
-  return YES;
+
+  return ([[NSUserDefaults standardUserDefaults] integerForKey:@"IsEditorMode"] != 1)
 }
 
 + (NSString *) md5:(NSString *)str {
